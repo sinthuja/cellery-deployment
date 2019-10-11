34fab81b287d699a996a2fd0bb0027b4c1265a54
sinthuja <s.rajan.sinthu@gmail.com>
2019-10-11 17:11:52 +0530

    Add kind k8s cluster configurations

---
+++ kind/kind-proxy.sh
@@ -0,0 +1,10 @@
+for port in 80 443
+do
+    node_port=$(kubectl get service -n ingress-nginx ingress-nginx -o=jsonpath="{.spec.ports[?(@.port == ${port})].nodePort}")
+    docker rm banzai-kind-proxy-${port}
+    docker run -d --name banzai-kind-proxy-${port} \
+      --publish 127.0.0.1:${port}:${port} \
+      --link cellery-control-plane:target \
+      alpine/socat -dd \
+      tcp-listen:${port},fork,reuseaddr tcp-connect:target:${node_port}
+done
